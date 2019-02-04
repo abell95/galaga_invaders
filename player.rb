6a4c2bf74106684917efbc5fe405e205c2723f44
abell95 <belland@oregonstate.edu>
2019-02-04 11:17:08 -0800

    Add basic logic for shooting and moving, references #6

--- player.rb
+++ player.rb
@@ -1,16 +1,28 @@
 
 class PlayerShip
-  def initialize(position, dimensions)
+  def initialize(position, hitbox)
     @position = position
-	 @dimensions = dimensions
+	  @hitbox = hitbox
   end
   
   def move(position)
-  
+    @position = position
   end
   
-  def shoot()
-  
+  def shoot
+    bullet = Bullet.new(@position)
+    bullet.fire()
+  end
+
+  def key_pressed(key)
+    case key
+    when 'a'
+      move(-10)
+    when 'd'
+      move(10)
+    when 'Space'
+      shoot()
+    end
   end
   
 end
