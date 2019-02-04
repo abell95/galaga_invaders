a8390ae245a8d5c830a03297bdbc4f96fcb32c7d
bmchristensen <bmchristensen@gmail.com>
2019-02-04 11:24:36 -0800

    Initialize game objects references #8

--- main.rb
+++ main.rb
@@ -2,6 +2,11 @@ class GalagaInvaders
   def initialize
     @caption = "Galaga Invaders"
     @active = false
+    @ship.Ship.new
+    @fort.Fort.new
+    @alien.Alien.new
+    @ufo.Ufo.new
+    @big_alien.BigAlien.new
   end
 
   def update
