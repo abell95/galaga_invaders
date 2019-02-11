9f41166895d8b12f558045d68b15b4a474b816de
bmchristensen <bmchristensen@gmail.com>
2019-02-11 11:32:19 -0800

    Change dot operator to assignment. References #8

--- main.rb
+++ main.rb
@@ -2,11 +2,11 @@ class GalagaInvaders
   def initialize
     @caption = "Galaga Invaders"
     @active = false
-    @ship.Ship.new
-    @fort.Fort.new
-    @alien.Alien.new
-    @ufo.Ufo.new
-    @big_alien.BigAlien.new
+    @ship = Ship.new
+    @fort = Fort.new
+    @alien =Alien.new
+    @ufo = Ufo.new
+    @big_alien = BigAlien.new
   end
 
   def update
