a9cadbddcb3b9c0d2c9284a60c8382f474f53454
bmchristensen <bmchristensen@gmail.com>
2019-03-06 11:46:30 -0800

    Stub time class

---
+++ time.rb
@@ -0,0 +1,16 @@
+class Time
+  attr_accessor :wave
+
+  def initialize(wave)
+    @wave = wave
+  end
+
+  def level_change
+    wave.empty
+    increase_level
+  end
+
+  def increase_level
+    spawn_next_wave()
+  end
+end
